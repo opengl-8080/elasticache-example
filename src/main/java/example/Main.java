473dfff677b6aae54d62c5e2c641900297590aa9
opengl-8080 <opengl-8080@users.noreply.github.com>
2020-06-25 11:34:51 +0900

    initialize

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,37 @@
+package example;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.sync.RedisCommands;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Map<String, String> argsMap = parse(args);
+        final String scheme = argsMap.getOrDefault("scheme", "redis");
+        final String host = argsMap.getOrDefault("host", "localhost");
+        final String port = argsMap.getOrDefault("port", "6379");
+        final String password = argsMap.containsKey("password") ? argsMap.get("password") + "@" : "";
+        
+        final RedisURI uri = RedisURI.create(String.format("%s://%s%s:%s", scheme, password, host, port));
+        final RedisClient client = RedisClient.create(uri);
+        final RedisCommands<String, String> commands = client.connect().sync();
+        commands.set("message", "Hello at " + LocalDateTime.now());
+        final String message = commands.get("message");
+        System.out.println("message = " + message);
+        client.shutdown();
+    }
+    
+    private static Map<String, String> parse(String[] args) {
+        Map<String, String> argsMap = new HashMap<>();
+        for (String arg : args) {
+            final String[] tokens = arg.split("=");
+            argsMap.put(tokens[0], tokens[1]);
+        }
+        return argsMap;
+    }
+}
